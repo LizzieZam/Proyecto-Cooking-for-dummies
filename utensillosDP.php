9a93abfe54a00a2092813875441f296684453e45
LizzieZam <43660974+LizzieZam@users.noreply.github.com>
2018-10-19 08:26:08 -0500

    Update utensillosDP.php

--- utensillosDP.php
+++ utensillosDP.php
@@ -7,14 +7,32 @@ use Illuminate\Http\Request;
 class utencillosDP extends Controller
 {
 
+	 function create()
+    {
+       $utencillo=new utencillo($description,$useful,$tipo);
+		$utencillo->createUtencillo($description,$useful,$tipo);
+		return view('utencillo',['utencilloBlade'=>$utensillo]);
+    }
 	function tipoComida($tipo){
 		
 		if($tipo!=="asiatico")
 			$tipoUtensillo='Palillos';
 		else
 			$tipoUtensillo='Cubiertos';
-		return view('utencillo',['comidaBlade'=>$tipo,'tipoUtencilloBlade'=>$tipoUtensillo]);
+		$mensaje='El utencillo '.$description.' a sido creado exitosamente';
+		return view('utencillo',['mensajeBlade'=>$mensaje]);
 	}
-
+	function update($id,$description,$useful,$tipo){
+		$utencillo=new utencillo();
+		$utencillo->updateUtencillo($id,$description,$useful,$tipo);
+		$mensaje='El utencillo '.$description.' a sido modificado exitosamente';
+		return view('utencillo',['mensajeBlade'=>$mensaje]);
+	}
+	function delete($id){
+		$utencillo=new utencillo();
+		$utencillo->deleteUtencillo($id);
+		$mensaje='El utencillo a sido eliminado exitosamente';
+		return view('utencillo',['mensajeBlade'=>$mensaje]);
+	}
     
 }
